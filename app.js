3c5e1a93763aaedd859239281042ecf5ed0b7e9d
Medhir Bhargava <medhirbhargava@gmail.com>
2015-11-23 22:46:47 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var app = angular.module('directoryApp', ['ListingsFactory']);
+
+app.controller('listingsController', function($scope, Listings) {
+  $scope.listings = Listings;
+  $scope.detailedInfo = undefined;
+
+  /* 
+    Implement these functions in the controller to make your application function 
+    as described in the assignment spec. 
+   */
+  $scope.addListing = function() {};
+  $scope.deleteListing = function() {};
+  $scope.showDetails = function(index) {};
+});
