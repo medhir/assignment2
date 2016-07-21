1f7d491b8c40f12e3e78c09d7ed70dfcd3d7071d
Medhir Bhargava <medhirbhargava@gmail.com>
2016-07-21 10:53:52 -0700

    add README content, other repository modifications

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-var app = angular.module('directoryApp', ['ListingsFactory']);
+/* register the modules the application depends upon here*/
+angular.module('listings', []);
 
-app.controller('listingsController', function($scope, Listings) {
-  $scope.listings = Listings;
-  $scope.detailedInfo = undefined;
-
-  /* 
-    Implement these functions in the controller to make your application function 
-    as described in the assignment spec. 
-   */
-  $scope.addListing = function() {};
-  $scope.deleteListing = function(index) {};
-  $scope.showDetails = function(index) {};
-});
+/* register the application and inject all the necessary dependencies */
+var app = angular.module('directoryApp', ['listings']);
